13fd98fcb15f4bbdb2cf5c846b325d3e873c38b2
johannetronstad <johanne.tronstad@nav.no>
2026-05-21 11:59:40 +0200

    oppdater pdfgen til versjon 2.0.119 i Dockerfile

--- run_dev.sh
+++ run_dev.sh
@@ -6,9 +6,10 @@ docker run \
         -v $CURRENT_PATH/templates:/app/templates \
         -v $CURRENT_PATH/fonts:/app/fonts \
         -v $CURRENT_PATH/data:/app/data \
+        -v $CURRENT_PATH/resources:/app/resources \
         -p 8080:8080 \
         -e DISABLE_PDF_GET=false \
         -e JDK_JAVA_OPTIONS \
         -it \
         --rm \
-        ghcr.io/navikt/pdfgen:2.0.22
+        ghcr.io/navikt/pdfgen:2.0.119
